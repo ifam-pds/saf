ba0d8599060a83186f24970e193493d4735fd5de
Juliano Toriani <julianotoriani@gmail.com>
2016-11-05 17:31:03 -0400

    adição do atributo de status à classe item

---
+++ common/src/main/java/br/edu/ifam/saf/enums/StatusItem.java
@@ -0,0 +1,15 @@
+package br.edu.ifam.saf.enums;
+
+public enum StatusItem {
+    ATIVO("Ativo"), INATIVO("Inativo");
+
+    private String descricao;
+
+    StatusItem(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
