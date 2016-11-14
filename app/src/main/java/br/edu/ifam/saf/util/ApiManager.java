25d3e6f8477abaea4f9ff872ee77228209bc83ff
Juliano Toriani <julianotoriani@gmail.com>
2016-11-13 21:15:22 -0400

    mapeamento de datas

--- app/src/main/java/br/edu/ifam/saf/util/ApiManager.java
+++ app/src/main/java/br/edu/ifam/saf/util/ApiManager.java
@@ -2,10 +2,18 @@ package br.edu.ifam.saf.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.TypeAdapter;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import br.edu.ifam.saf.SAFService;
 import br.edu.ifam.saf.api.data.MensagemErroResponse;
@@ -24,7 +32,7 @@ public final class ApiManager {
 
 
     private static final Gson GSON = new GsonBuilder()
-//            .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+            .registerTypeAdapter(Date.class, new DateTypeDeserializer())
             .create();
 
     private static SAFService safService;
@@ -46,8 +54,8 @@ public final class ApiManager {
                 .addInterceptor(new Interceptor() {
                     @Override
                     public okhttp3.Response intercept(Chain chain) throws IOException {
-                        if (repository.getUserInfo() != null) {
-                            Request request = chain.request().newBuilder().header("Authorization", "Bearer " + repository.getUserInfo().getToken()).build();
+                        if (repository.getInfoUsuario() != null) {
+                            Request request = chain.request().newBuilder().header("Authorization", "Bearer " + repository.getInfoUsuario().getToken()).build();
                             return chain.proceed(request);
                         }
                         return chain.proceed(chain.request());
@@ -67,7 +75,7 @@ public final class ApiManager {
 
     }
 
-    public static SimpleDateFormat getFormatter(){
+    public static SimpleDateFormat getFormatter() {
 
         if (formatter == null) {
             formatter = new SimpleDateFormat("dd/MM HH:mm");
@@ -95,6 +103,34 @@ public final class ApiManager {
 
     public static Gson getGson() {
         return GSON;
+    }
+
+    static class DateTypeDeserializer implements JsonDeserializer<Date> {
+        private static final String[] DATE_FORMATS = new String[]{
+                "yyyy-MM-dd'T'HH:mm:ssZ",
+                "yyyy-MM-dd'T'HH:mm:ss",
+                "yyyy-MM-dd",
+                "EEE MMM dd HH:mm:ss z yyyy",
+                "HH:mm:ss",
+                "MM/dd/yyyy HH:mm:ss aaa",
+                "yyyy-MM-dd'T'HH:mm:ss.SSSSSS",
+                "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS",
+                "yyyy-MM-dd'T'HH:mm:ss.SSSSSSS'Z'",
+                "MMM d',' yyyy H:mm:ss a",
+                "MMM d',' yyyy H:mm:ss"
+        };
+
+        @Override
+        public Date deserialize(JsonElement jsonElement, Type typeOF, JsonDeserializationContext context) throws JsonParseException {
+            for (String format : DATE_FORMATS) {
+                try {
+                    return new SimpleDateFormat(format, Locale.US).parse(jsonElement.getAsString());
+                } catch (ParseException e) {
+                }
+            }
+            throw new JsonParseException("Unparseable date: \"" + jsonElement.getAsString() + '"');
+        }
+
     }
 
 }
