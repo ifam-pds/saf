1d6a88005bacd311b1dfac3c4b577a3d85e28be9
Juliano Toriani <julianotoriani@gmail.com>
2016-11-17 14:54:40 -0400

    definição de níveis de perfil

--- common/src/main/java/br/edu/ifam/saf/enums/Perfil.java
+++ common/src/main/java/br/edu/ifam/saf/enums/Perfil.java
@@ -1,5 +1,15 @@
 package br.edu.ifam.saf.enums;
 
 public enum Perfil {
-    CLIENTE, FUNCIONARIO, ADMINISTRADOR
+    CLIENTE(0), FUNCIONARIO(1), ADMINISTRADOR(2);
+
+    private final int nivel;
+
+    Perfil(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
 }
